5129c40a5362dff9d3057e8065a64dde0634a109
Muse <526659043@qq.com>
2016-07-11 14:31:13 +0800

    Make atomic to header, cut cpp

---
+++ include/satomic.h
@@ -0,0 +1,79 @@
+/*---------------------------------------------
+ *     modification time: 2016-07-11 12:57:45
+ *     mender: Muse
+-*---------------------------------------------*/
+
+/*---------------------------------------------
+ *     file: ato.h
+ *     creation time: 2016-07-11 12:57:45
+ *     author: Muse 
+-*---------------------------------------------*/
+
+/*---------------------------------------------
+ *       Source file content Five part
+ *
+ *       Part Zero:  Include
+ *       Part One:   Define 
+ *       Part Two:   Typedef
+ *       Part Three: Struct
+ *
+ *       Part Four:  Function
+ *
+-*---------------------------------------------*/
+
+/*---------------------------------------------
+ *             Part Zero: Include
+-*---------------------------------------------*/
+
+#include <stdbool.h>
+
+
+/*---------------------------------------------
+ *            Part Two: Typedef
+-*---------------------------------------------*/
+
+typedef struct matos    MATOS;
+
+
+/*---------------------------------------------
+ *            Part Three: Struct
+-*---------------------------------------------*/
+
+struct  matos {
+    int32_t cnt; 
+};
+
+
+/*---------------------------------------------
+ *            Part Four: Function
+-*---------------------------------------------*/
+
+#define mato_init(ato, set) \
+        ato.cnt = set;
+
+#define mato_inc(ato) \
+        __sync_fetch_and_add(ato.cnt, 1)
+
+#define mato_add(ato, inc) \
+        __sync_fetch_and_add(ato.cnt, (inc))
+
+#define mato_dec(ato) \
+        __sync_fetch_and_sub(ato.cnt, 1)
+
+#define mato_sub(ato, sub) \
+        __sync_fetch_and_sub(ato.cnt, (sub))
+
+#define mato_inc_and_test(ato) \
+        ((__sync_fetch_and_sub(ato.cnt, 1) - 1) ? false : true)
+
+#define mato_add_and_test(ato, inc) \
+        ((__sync_fetch_and_add(ato.cnt, (inc)) + inc) ? false : true)
+
+#define mato_dec_and_test(ato) \
+        ((__sync_fetch_and_sub(ato.cnt, 1) - 1) ? false : true)
+
+#define mato_sub_and_test(ato, sub) \
+        ((__sync_fetch_and_sub(ato.cnt, (sub)) - sub) ? false : true)
+
+
+#endif
